55c528c9213a9bf17bbbd804939d2d5e6dbd56b3
Marcin Lapaj <cod3r@debian.guus.ru>
2015-09-25 05:37:52 -0400

    first lines of fourier code

--- DSP/fourier.cxx
+++ DSP/fourier.cxx
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cmath>
 #include "fourier.hxx"
-
 
 using namespace std;
 fourier::fourier(int nPoints){
 	LOG4CPLUS_DEBUG(logger, "Starting constructor...");
 	this->nPoints = nPoints;
+	w = exp(complex<double>(0,2*M_PI/(double)nPoints));
+	cout << w << endl;
 }
--- DSP/fourier.hxx
+++ DSP/fourier.hxx
@@ -4,11 +4,17 @@
 #include <log4cplus/configurator.h>
 #include <log4cplus/loggingmacros.h>
 #include <iomanip>
+#include <complex>
+
+using namespace std;
 using namespace log4cplus;
 
 class fourier{
 	private:
+		// for fourier math
 		int nPoints;
+		complex<double> w;
+		// other
 		Logger logger = Logger::getInstance("fourier");
 	public:
 		fourier(int nPoints);
