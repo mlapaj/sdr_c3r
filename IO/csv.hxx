bffb5ba5e5d4fa5a517ebd305a257e714356679a
Marcin Lapaj <marcin.lapaj@gmail.com>
2015-11-17 12:04:37 +0100

    csv i/o now are handled in header file only

--- IO/csv.hxx
+++ IO/csv.hxx
@@ -1,20 +1,88 @@
 #pragma once
 #include <iomanip>
 #include <string>
-#include <iostream>
 #include <complex>
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-class csv{
-	private:
-		static const int blockSize = 1024;
-		static char block[blockSize];
-	public:
-		static int read(string filename,vector<int> &array);
-		static int read(string filename,vector<double> &array,int howMany = 0);
-		static int read(string filename,vector<complex<double>> &array,int howMany = 0);
-		static int save(string filename,vector<double> &array,int howMany = 0);
-		static int save(string filename,vector<complex<double>> &array,int howMany = 0);
-};
+namespace csv{
+	template <typename T>
+		int read(string filename,vector<complex<T>> &array,int howMany = 0){
+			ifstream file(filename);
+			if (file.is_open()){
+				int timer = howMany;
+				while (!file.eof()){
+					T i = 0;
+					T j = 0;
+					char c;
+					file >> i;
+					file >> j;
+					file >> c;
+					file >> c;
+					if ((howMany > 0) && (--timer < 0)) break;
+					array.push_back(complex<T>(i,j));
+				}
+			}
+			return 0;
+		}
+
+	template <typename T>
+		int read(string filename,vector<T> &array,int howMany = 0){
+			ifstream file(filename);
+			if (file.is_open()){
+				int timer = howMany;
+				while (!file.eof()){
+					T i;
+					char c;
+					file >> i;
+					file >> c;
+					if ((howMany > 0) && (--timer < 0)) break;
+					array.push_back(i);
+				}
+			}
+			return 0;
+		}
+
+
+	template <typename T>
+		int save(string filename,vector<complex<T>> &array,int howMany = 0){
+			ofstream file(filename);
+			if (file.is_open()){
+				int count = 0;
+				while (count < array.size()){
+					T real = array[count].real();
+					T imag = array[count].imag();
+
+					char c = ',';
+					file << real;
+					file << showpos << imag << "i";
+					if ((howMany > 0) && (++count >= howMany)) break;
+					file << c;
+				}
+			}
+			return 0;
+		}
+
+	template <typename T>
+		int save(string filename,vector<T> &array,int howMany = 0){
+			ofstream file(filename);
+			if (file.is_open()){
+				int count = 0;
+				while (count < array.size()){
+					T i = array[count];
+					char c = ',';
+					file << i;
+					if ((howMany > 0) && (++count >= howMany)) break;
+					file << c;
+				}
+			}
+			return 0;
+		}
+
+
+}
+
+
